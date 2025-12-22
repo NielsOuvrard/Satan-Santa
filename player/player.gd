af1d0efde1b43c5514006dd311a91df0c7b4caaa
Niels Ouvrard <91876336+NielsOuvrard@users.noreply.github.com>
2025-12-22 11:13:30 +0100

    Refactor scene setup and player movement logic

--- player/player.gd
+++ player/player.gd
@@ -2,16 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+@onready var player: Sprite2D = $Player
 
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_x := Input.get_axis("ui_left", "ui_right")
+	var input_y := Input.get_axis("ui_up", "ui_down")
+	var dir := Vector2(input_x, input_y)
 	
-	if direction:
-		velocity = direction * SPEED
+	if dir:
+		velocity = dir * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+	
+	if input_x > 0:
+		player.flip_h = false
+	else:
+		player.flip_h = true
 
 	move_and_slide()
