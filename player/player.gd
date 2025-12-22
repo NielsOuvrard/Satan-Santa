4ae8b4e4a1e20825335f6be20eeaa574f7d4735b
Niels Ouvrard <91876336+NielsOuvrard@users.noreply.github.com>
2025-12-22 23:22:53 +0100

    controller and mouse OK

--- player/player.gd
+++ player/player.gd
@@ -24,7 +24,7 @@ func _on_torch_visibility_changed(should_be_visible: bool) -> void:
 
 func _physics_process(_delta: float) -> void:
 	# Handle torch toggle
-	if has_torch and Input.is_action_just_pressed("ui_accept"):  # Space key
+	if has_torch and Input.is_action_just_pressed("toggle_torch"):
 		torch_on = !torch_on
 	
 	# Drain torch energy when on
@@ -39,33 +39,72 @@ func _physics_process(_delta: float) -> void:
 		torch_energy = min(max_torch_energy, torch_energy + (torch_drain_rate / 4) * _delta)  # Recharge when off
 		SignalHandler.update_torch_energy(torch_energy, max_torch_energy)
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_x := Input.get_axis("ui_left", "ui_right")
-	var input_y := Input.get_axis("ui_up", "ui_down")
-	var dir := Vector2(input_x, input_y)
+	# Movement input - supports both keyboard and gamepad
+	var input_x := 0.0
+	var input_y := 0.0
 	
+	# Keyboard/D-pad input
+	if Input.is_action_pressed('ui_right'):
+		input_x += 1
+	if Input.is_action_pressed('ui_left'):
+		input_x -= 1
+	if Input.is_action_pressed('ui_down'):
+		input_y += 1
+	if Input.is_action_pressed('ui_up'):
+		input_y -= 1
+	
+	# Gamepad analog stick for movement
+	if Input.is_action_pressed('move_right'):
+		input_x += Input.get_action_strength('move_right')
+	if Input.is_action_pressed('move_left'):
+		input_x -= Input.get_action_strength('move_left')
+	if Input.is_action_pressed('move_down'):
+		input_y += Input.get_action_strength('move_down')
+	if Input.is_action_pressed('move_up'):
+		input_y -= Input.get_action_strength('move_up')
+	
+	var dir := Vector2(input_x, input_y)
 	if dir != Vector2.ZERO:
+		dir = dir.normalized()
 		velocity = dir * SPEED
 		
 		if input_x > 0:
 			player.flip_h = false
-		else:
+		elif input_x < 0:
 			player.flip_h = true
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
-	# Rotate torch and point light to follow the mouse every frame.
-	var mouse_pos: Vector2 = get_global_mouse_position()
-	var to_mouse: Vector2 = mouse_pos - torch_handled.global_position
-	if to_mouse.length() > 0.001:
-		var angle := to_mouse.angle()
-		# If the torch sprite is flipped horizontally, mirror the rotation so
-		# the visual still points at the mouse.
+	# Aiming - check for gamepad right stick input first
+	var aim_direction := Vector2.ZERO
+	
+	# Gamepad right stick aiming
+	if Input.is_action_pressed('look_right'):
+		aim_direction.x += Input.get_action_strength('look_right')
+	if Input.is_action_pressed('look_left'):
+		aim_direction.x -= Input.get_action_strength('look_left')
+	if Input.is_action_pressed('look_down'):
+		aim_direction.y += Input.get_action_strength('look_down')
+	if Input.is_action_pressed('look_up'):
+		aim_direction.y -= Input.get_action_strength('look_up')
+	
+	# If gamepad stick is being used, use that direction
+	if aim_direction.length() > 0.1:  # Deadzone
+		aim_direction = aim_direction.normalized()
+		var angle := aim_direction.angle()
 		if torch_handled.flip_h:
 			torch_handled.rotation = PI - angle
 		else:
 			torch_handled.rotation = angle
-		# Keep the light's rotation matching the computed angle.
+	else:
+		# Otherwise use mouse position
+		var mouse_pos: Vector2 = get_global_mouse_position()
+		var to_mouse: Vector2 = mouse_pos - torch_handled.global_position
+		if to_mouse.length() > 0.001:
+			var angle := to_mouse.angle()
+			if torch_handled.flip_h:
+				torch_handled.rotation = PI - angle
+			else:
+				torch_handled.rotation = angle
 
 	move_and_slide()
