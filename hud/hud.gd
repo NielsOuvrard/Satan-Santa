f9fe1a1c71c850ab06580a81c3c7e6aa7f5b8cdf
Niels Ouvrard <91876336+NielsOuvrard@users.noreply.github.com>
2025-12-23 11:34:01 +0100

    {FEAT} add key display functionality on HUD

--- hud/hud.gd
+++ hud/hud.gd
@@ -4,20 +4,22 @@ extends CanvasLayer
 
 @onready var torch_display: HBoxContainer = $MarginContainer/HBoxContainer/LeftPanel/TorchDisplay
 @onready var torch_progress: ProgressBar = $MarginContainer/HBoxContainer/LeftPanel/TorchDisplay/TorchProgress
+@onready var key_display: HBoxContainer = $MarginContainer/HBoxContainer/RightPanel/KeyDisplay
 
-var keys_collected: int = 0
 var max_torch_energy: float = 100.0
 var current_torch_energy: float = 100.0
 
 
 func _ready() -> void:
 	update_torch_display()
+	update_key_display()
 	
 	# Connect to signals
 	SignalHandler.torch_visibility_changed.connect(_on_torch_obtained)
 	SignalHandler.torch_energy_changed.connect(_on_torch_energy_changed)
 	SignalHandler.player_caught.connect(_on_player_caught)
-	# SignalHandler.key_collected.connect(_on_key_collected)
+	SignalHandler.key_collected.connect(_on_key_collected)
+	SignalHandler.computer_unlocked.connect(_on_computer_unlocked)
 	
 	_setup_screamer()
 
@@ -80,8 +82,40 @@ func _on_torch_obtained(visible: bool) -> void:
 	torch_display.visible = visible
 
 
-func _on_key_collected() -> void:
-	keys_collected += 1
+func _on_key_collected(key_id: int) -> void:
+	update_key_display()
+
+
+func _on_computer_unlocked(computer_id: int) -> void:
+	update_key_display()
+
+
+func update_key_display() -> void:
+	# Clear existing key sprites
+	for child in key_display.get_children():
+		child.queue_free()
+	
+	# Get keys that are collected but not yet used
+	var available_keys = []
+	for key_id in Data.keyes_unlocked:
+		if key_id not in Data.unlocked_computers:
+			available_keys.append(key_id)
+	
+	# Show key display only if there are keys
+	key_display.visible = available_keys.size() > 0
+	
+	# Create sprite for each available key
+	var position_offset = 0
+	for key_id in available_keys:
+		var key_sprite = Sprite2D.new()
+		key_sprite.texture = load("res://objects/key/keyes.png")
+		key_sprite.vframes = 4
+		key_sprite.frame = key_id
+		key_sprite.rotation = PI / 2  # 90 degrees rotation
+		key_sprite.scale = Vector2(15, 15)
+		key_sprite.position = Vector2(position_offset, 0)
+		position_offset -= key_sprite.texture.get_width() * key_sprite.scale.x + 10
+		key_display.add_child(key_sprite)
 
 
 func _on_torch_energy_changed(energy: float, max_energy: float) -> void:
