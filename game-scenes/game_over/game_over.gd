f863f4a2f4df7fb3a04052bbd56bf3032b7fdcd8
Sookyuu <max.roiron@epitech.eu>
2025-12-24 11:12:33 +0100

    {FIX} game over boutton fixed

--- game-scenes/game_over/game_over.gd
+++ game-scenes/game_over/game_over.gd
@@ -1,8 +1,12 @@
 extends Control
 
 
+func _ready() -> void:
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _on_retry_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game-scenes/world_1/world_1.tscn")
+	get_tree().change_scene_to_file("res://game-scenes/world_3/world_3.tscn")
 
 
 func _on_main_menu_button_pressed() -> void:
